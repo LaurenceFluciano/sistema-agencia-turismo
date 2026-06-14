7bc66ff90d5809f700851754bf15668a0476c198
LaurenceFluciano <laurencefluciano@gmail.com>
2026-06-13 23:12:01 -0300

    adicionar tipos no banco de dados.

---
+++ database/000_tipos.down.sql
@@ -0,0 +1,8 @@
+DROP TYPE IF EXISTS tipo_status_pessoa CASCADE; 
+DROP TYPE IF EXISTS tipo_status_pacote CASCADE; 
+DROP TYPE IF EXISTS tipo_status_reserva CASCADE; 
+DROP TYPE IF EXISTS tipo_status_pagamento CASCADE; 
+DROP TYPE IF EXISTS tipo_status_fornecedor_servico CASCADE; 
+DROP TYPE IF EXISTS tipo_status_itinerario CASCADE; 
+DROP TYPE IF EXISTS tipo_status_servico CASCADE; 
+DROP TYPE IF EXISTS tipo_status_reserva_item CASCADE; 
---
+++ database/000_tipos.up.sql
@@ -0,0 +1,43 @@
+CREATE TYPE tipo_status_pessoa AS ENUM (
+  'ATIVO', 
+  'INATIVO'
+);
+CREATE TYPE tipo_status_pacote AS ENUM (
+  'DISPONIVEL',
+  'INDISPONIVEL',
+  'ENCERRADO'
+);
+CREATE TYPE tipo_status_reserva AS ENUM (
+  'RASCUNHO', 
+  'CONFIRMADA', 
+  'CANCELADA', 
+  'PENDENTE', -- significado semantico de pendente é quando esperamos os vouchers serem efetuados
+  'CONCLUIDA',
+  'EM_ATRASO'
+);
+CREATE TYPE tipo_status_pagamento AS ENUM (
+  'PAGO', 
+  'PENDENTE', 
+  'VENCIDO'
+);
+CREATE TYPE tipo_status_fornecedor_servico AS ENUM (
+  'DISPONIVEL',
+  'PAUSADO',
+  'ENCERRADA',
+  'CANCELADA'
+);
+CREATE TYPE tipo_status_itinerario AS ENUM (
+  'RASCUNHO',
+  'AGENDADO',
+  'EM_ANDAMENTO',
+  'CONCLUIDO',
+  'SUSPENSO',
+  'CANCELADO'
+);
+CREATE TYPE tipo_status_servico AS ENUM (
+  'ATIVO',
+  'INATIVO',
+  'MANUTENCAO',
+  'INTERDITADO',
+  'EM_ANALISE'
+);
