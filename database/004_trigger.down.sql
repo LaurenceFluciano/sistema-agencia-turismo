33d0b68331e25b028dce70645ef1621fab21693d
LaurenceFluciano <laurencefluciano@gmail.com>
2026-06-13 23:13:10 -0300

    adicionar trigger de negocio e operacionais.

---
+++ database/004_trigger.down.sql
@@ -0,0 +1,16 @@
+-- Gatilhos de atualização de timestamp
+DROP TRIGGER IF EXISTS tg_atualiza_data_atualizacao_servico ON "Servico";
+DROP TRIGGER IF EXISTS tg_atualiza_data_atualizacao_fornecedor_servico ON "Fornecedor_Servico";
+
+-- Gatilho de validação de papéis
+DROP TRIGGER IF EXISTS tg_verificar_papel_tipo_pessoa ON "Pessoa_Papel";
+
+-- Gatilhos de gerenciamento de pacotes na reserva
+DROP TRIGGER IF EXISTS tg_copiar_itens_pacote_reserva ON "Reserva";
+DROP TRIGGER IF EXISTS tg_remover_itens_pacote_reserva ON "Reserva";
+
+-- Gatilho de cálculo financeiro do total da reserva
+DROP TRIGGER IF EXISTS tg_atualizar_total_reserva ON "Reserva_Item";
+
+-- Gatilho de verificação de inadimplência/quitação
+DROP TRIGGER IF EXISTS tg_marcar_reserva_como_concluido_ou_em_atraso ON "Pagamento";
---
+++ database/004_trigger.up.sql
@@ -0,0 +1,42 @@
+CREATE OR REPLACE TRIGGER tg_atualiza_data_atualizacao_servico
+BEFORE UPDATE ON "Servico"
+FOR EACH ROW
+EXECUTE FUNCTION fn_atualiza_data_atualizacao();
+
+
+CREATE OR REPLACE TRIGGER tg_atualiza_data_atualizacao_fornecedor_servico
+BEFORE UPDATE ON "Fornecedor_Servico"
+FOR EACH ROW
+EXECUTE FUNCTION fn_atualiza_data_atualizacao();
+
+
+CREATE OR REPLACE TRIGGER tg_verificar_papel_tipo_pessoa
+BEFORE UPDATE OR INSERT ON "Pessoa_Papel"
+FOR EACH ROW
+EXECUTE FUNCTION fn_validar_papel_pessoa();
+
+
+CREATE OR REPLACE TRIGGER tg_copiar_itens_pacote_reserva
+AFTER INSERT ON "Reserva"
+FOR EACH ROW
+WHEN (NEW."id_pacote" IS NOT NULL)
+EXECUTE FUNCTION fn_copiar_itens_pacote_para_reserva();
+
+
+CREATE OR REPLACE TRIGGER tg_remover_itens_pacote_reserva
+AFTER UPDATE ON "Reserva"
+FOR EACH ROW
+WHEN (NEW."id_pacote" IS NULL)
+EXECUTE FUNCTION fn_remover_itens_pacote_da_reserva();
+
+
+CREATE OR REPLACE TRIGGER tg_atualizar_total_reserva
+AFTER UPDATE OR INSERT OR DELETE ON "Reserva_Item"
+FOR EACH ROW
+EXECUTE FUNCTION fn_atualizar_total_reserva();
+
+
+CREATE OR REPLACE TRIGGER tg_marcar_reserva_como_concluido_ou_em_atraso
+AFTER UPDATE OR INSERT ON "Pagamento"
+FOR EACH ROW
+EXECUTE FUNCTION fn_marcar_reserva_como_concluido_ou_em_atraso();
